a1e07d1f940ed2adbb426b320780789f80092cd7
Arc1el <khm970514@gmail.com>
2022-07-11 12:29:33 +0900

    arduino wifi ble json test

---
+++ arduino/wifi_ble_json_test/wifi_ble_json_test.ino
@@ -0,0 +1,80 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <ArduinoBLE.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "DfXLabPros_2.4G";
+const char* pass = "thdtnrms1!";
+int status = WL_IDLE_STATUS;
+
+int a;
+int b;
+
+void setup() {
+  Serial.begin(9600);
+
+  //try wifi module check
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("WiFi module initiation error");
+    while(true);
+  }
+
+  //try wifi module initiation
+  while (status != WL_CONNECTED) {
+    Serial.print("try to connect WPA SSID : ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(5000);
+  }
+
+  //wifi connect success
+  Serial.println("connected to Network");
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address : ");
+  Serial.println(ip);
+
+  //try ble module check
+  if(!BLE.begin()){
+    Serial.println("BLE start failed");
+    while(1);
+  }
+
+  //try to set BLE device
+  BLE.setLocalName("ArduinoNano33IoTBLE");
+  BLE.setAdvertisedServiceUuid("abf55695-4242-4cd8-af5b-cdb4a069e6b8");
+
+  //start Advertising
+  BLE.advertise();
+  Serial.println("Blutooth device is activated. Waiting for connections");
+
+  a = 1;
+  b = 1;
+}
+
+void loop() {
+  //wait for BLE central. if connection is success, Serial print the BLE central address
+  BLEDevice central = BLE.central();
+  if (central) {
+    Serial.print("Connected to central : ");
+    Serial.println(central.address());
+
+    //BLE is connected
+    while (central.connected()) {
+      Serial.println("Do something");
+      
+      String jsondata = "";
+      StaticJsonBuffer jsonBuffer;
+      JsonObject& json = jsonBuffer.createObject();
+
+      json["a"] = a;
+      json["b"] = b;
+
+      json.printTo(jsondata);
+      Serial.println(jsondata);
+
+      a++;
+      b++;
+      delay(1000);
+    }
+  }
+}
