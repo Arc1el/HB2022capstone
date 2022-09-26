7ebc30e943a1827aec92754af807102009788149
Arc1el <khm970514@gmail.com>
2022-09-26 13:25:09 +0900

    Update esp32-hx711.ino
    add fare flag

--- arduino/esp32-hx711/esp32-hx711.ino
+++ arduino/esp32-hx711/esp32-hx711.ino
@@ -5,6 +5,7 @@
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 16;
 const int LOADCELL_SCK_PIN = 4;
+bool TARE_FLAG = false;
 
 HX711 scale;
 
@@ -12,16 +13,21 @@ void setup() {
   Serial.begin(115200);
   rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  if (scale.is_ready()) {
-    scale.set_scale();    
-    scale.tare();
-  }
+
 }
 
 void loop() {
 
-  if (scale.is_ready()) {
-    //long reading = scale.get_units(10);
+  if (scale.is_ready() && TARE_FLAG == false) {
+    scale.set_scale();    
+    Serial.println("Tare... remove any weights from the scale.");
+    delay(3000);
+    scale.tare();
+    Serial.println("Tare done...");
+    TARE_FLAG = true;
+  }
+  
+  if (scale.is_ready() && TARE_FLAG == true) {
     long reading = scale.read();
     Serial.print("Result: ");
     Serial.println(reading);
@@ -29,5 +35,5 @@ void loop() {
   else {
     Serial.println("HX711 not found.");
   }
-  delay(1000);
+  delay(300);
 }
